685fc77071ce6c0d280746a65603a58127b558e5
Agamwa <agamwa16@gmail.com>
2025-01-05 13:37:02 +0200

    designing the application

---
+++ app/src/main/java/agam/w/myproject/SplashActivity.java
@@ -0,0 +1,40 @@
+package agam.w.myproject;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.imageView2);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.my_anim);
+        animation.setDuration(8000);
+        logo.setAnimation(animation);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sfx);
+        mediaPlayer.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        },8000);
+
+    }
+}
